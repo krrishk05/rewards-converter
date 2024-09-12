87ab56af9502dcc924a9adb6946248b9cb5f0b66
Krrish Kapoor <kapoorkrrish7@gmail.com>
2024-09-12 00:37:30 -0400

    Add RewardValue class with cash-to-miles and miles-to-cash conversion

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+// RewardValue.java
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Constructor that accepts cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // Constructor that accepts miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * MILES_TO_CASH_RATE;
+        }
+    }
+
+    // Returns the cash value of the RewardValue
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    // Returns the miles value of the RewardValue
+    public double getMilesValue() {
+        return this.milesValue;
+    }
+}
